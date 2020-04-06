703c4b8c489f9e8659990a092cd7c6e5a66061ab
NellyBP <nelly.borjon@hotmail.fr>
2020-04-06 11:25:26 +0200

    css problems

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const theGame = new TheGame();
 const score = document.getElementById('score');
 const time = document.getElementById('time');
 const background = document.getElementById("background");
+const character = document.getElementById("character")
+const homeScreen = document.getElementById("home-screen")
+const gameScreen = document.getElementById("game-screen")
 
 function play(){
     theGame.startGame();
     background.classList.remove("background-home");
     background.classList.add("background-game");
+    console.log(homeScreen)
+    homeScreen.style.visibility="hidden";
+    gameScreen.style.visibility="visible";
+    score.innerText=theGame.score;
+    time.innerText=theGame.timeLeft+" sec";    
 }
 
 //let's awake the start button
 
 const startButton = document.querySelector(".start-button");
-console.log(startButton)
 startButton.addEventListener('click',play)
     
