74c26c372c0d72aff162fda2c10965559be71e22
NellyBP <nelly.borjon@hotmail.fr>
2020-04-08 14:05:48 +0200

    score fixed

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ let characterPosition = {x:0}
 const homeScreen = document.getElementById("home-screen")
 const gameScreen = document.getElementById("game-screen")
 const scoreScreen = document.getElementById("score-screen")
-const appleOne = document.querySelector(".apple.one");
-// const appleTwo = document.querySelector(".apple.two");
-// const appleThree = document.querySelector(".apple.three");
-// const appleFour = document.querySelector(".apple.four");
 const apples = document.querySelectorAll(".apple");
 const bunnies = document.querySelectorAll(".bunny");
 
@@ -26,9 +22,8 @@ function play(){
     score.innerText=theGame.score;
     introMusic.innerHTML=" "
     printTime(); 
-    movingCharacter ();    
+    movingCharacter();    
     applesFalling();
-    printScore();
     bunniesAttacking()
 }
 
@@ -54,7 +49,6 @@ startButton.addEventListener('click', ()=>{
 //moving the character
 function movingCharacter (){
     window.onkeydown =function(e){
-
     if (e.code==="ArrowRight") {
         characterPosition.x = characterPosition.x + 20;
       } else if (e.code==="ArrowLeft") {
@@ -71,12 +65,15 @@ function movingCharacter (){
 
 function appleFalling (apple){
     let appleRect = apple.getBoundingClientRect();
-    let screenLimit = document.body.getBoundingClientRect()
-    appleRect.y = appleRect.y + 10;
-    if (appleRect.y < (screenLimit.height-150)) {
+    let screenLimit = document.body.getBoundingClientRect();
+    console.log("falling")
+    // let screenLimitWidth = screenLimit.height
+    appleRect.y = appleRect.y + (makeRandomNumber(2,5));
+    if (appleRect.y < (screenLimit.height-120)) {
         apple.style.transform=`translateY(${appleRect.y}px)`;  
     } else {
-        apple.style.transform = "translateY(10px)"
+        apple.style.transform = `translateY(${makeRandomNumber(0,200)}px)`;
+        // apple.style.transform = `translateX(${makeRandomNumber(0,screenLimit.width)}px)`
     }
     collisionDetection(apple)
 };
@@ -85,23 +82,20 @@ function applesFalling (){
     for (let i = 0; i< apples.length; i++ ){
         appleFalling(apples[i])
     }
-    setTimeout(function() {
         window.requestAnimationFrame(applesFalling)
-    },100)
 }
  
 //bunnies attacking
 
 function bunnyAttacking (bunny){
     let bunnyRect = bunny.getBoundingClientRect();
-    let screenLimit = document.body.getBoundingClientRect()
-    bunnyRect.y = bunnyRect.y + 20;
+    let screenLimit = document.body.getBoundingClientRect();
+    bunnyRect.y = bunnyRect.y + 2;
     if (bunnyRect.y < (screenLimit.height-195)){
     bunny.style.transform=`translateY(${bunnyRect.y}px)`;
 } else{
-    bunny.style.transform = "translateY(10px)"
+    bunny.style.transform = `translateY(${makeRandomNumber(0,200)}px)`
 }
-
      collisionDetection(bunny)
 };
 
@@ -109,9 +103,7 @@ function bunniesAttacking (){
     for (let i = 0; i< bunnies.length; i++ ){
     bunnyAttacking(bunnies[i])
     }
-    setTimeout(function() {
         window.requestAnimationFrame(bunniesAttacking)
-    },100)
 }
 
 //collision detection 
@@ -121,15 +113,18 @@ function collisionDetection (object){
 let characterRect = character.getBoundingClientRect()
 let objectRect = object.getBoundingClientRect()
 
+
 if (characterRect.x < objectRect.x + objectRect.width &&
     characterRect.x + characterRect.width > objectRect.x &&
     characterRect.y < objectRect.y + objectRect.height &&
     characterRect.y + characterRect.height > objectRect.y) {
      if(objectRect.width<100){
          theGame.addPoint()
+         object.style.transform = `translateY(${makeRandomNumber(0,100)}px)`;
      }else{
          theGame.removePoint()
-     
+         object.style.transform = `translateY(${makeRandomNumber(0,100)}px)`
+
      }
  } printScore()
 }
@@ -141,12 +136,18 @@ function finishGame(){
     background.classList.add("background-score");
     gameScreen.style.visibility="hidden";  
     scoreScreen.style.visibility="visible"; 
-    introMusic.innerHTML=`<audio controls autoplay style="display: none">
-    <source src="./title-screen.mp3" type="audio/mpeg" > `
-    document.querySelector("h2").innerText=`You stole ${theGame.score} apples`
+    introMusic.innerHTML= `<audio controls autoplay style="display: none">
+    <source src="./audios/title-screen.mp3" type="audio/mpeg" >
+    </audio>`
+    document.querySelector("h2").innerText=`You stole ${theGame.score} apples`;
  }   
 
 
 // définir un aléatoire
 
-    
+function makeRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+  }  
+
